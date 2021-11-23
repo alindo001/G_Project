c0dc9c4e51962adbc18919b4e06f4e489dac02f0
Andres <andreslindo20@gmail.com>
2021-11-23 01:06:38 -0500

    Update

--- Translator.py
+++ Translator.py
@@ -1,22 +1,28 @@
-translated = int(input("Enter a number to output the Spanish translation "))
+translated = int(input("Enter a number 1 through 10 to output the Spanish translation "))
 
-if translated == 1:
-    print("Uno")
-if translated == 2:
-    print("Dos")
-if translated == 3:
-    print("Tres")
-if translated == 4:
-    print("Cuatro")
-if translated == 5:
-    print("Cinco")
-if translated == 6:
-    print("Seis")
-if translated == 7:
-    print("Siete")
-if translated == 8:
-    print("Ocho")
-if translated == 9:
-    print("Nueve")
-if translated == 10:
-    print("Dies")
+while translated != 0:
+
+    if translated == 1:
+        print("Uno")
+    if translated == 2:
+        print("Dos")
+    if translated == 3:
+        print("Tres")
+    if translated == 4:
+        print("Cuatro")
+    if translated == 5:
+        print("Cinco")
+    if translated == 6:
+        print("Seis")
+    if translated == 7:
+        print("Siete")
+    if translated == 8:
+        print("Ocho")
+    if translated == 9:
+        print("Nueve")
+    if translated == 10:
+        print("Dies")
+    if translated > 10:
+        print("Please Enter a number 1 through 10")
+
+    translated = int(input("Enter a number 1 through to output the Spanish translation "))
