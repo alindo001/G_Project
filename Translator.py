753a26d72e9a274abcc90b5f35df254f62d4bb03
Andres <andreslindo20@gmail.com>
2021-11-23 00:48:03 -0500

    test

--- Translator.py
+++ Translator.py
@@ -1,1 +1,22 @@
-print("Test")
+translated = int(input("Enter a number to output the Spanish translation "))
+
+if translated == 1:
+    print("Uno")
+if translated == 2:
+    print("Dos")
+if translated == 3:
+    print("Tres")
+if translated == 4:
+    print("Cuatro")
+if translated == 5:
+    print("Cinco")
+if translated == 6:
+    print("Seis")
+if translated == 7:
+    print("Siete")
+if translated == 8:
+    print("Ocho")
+if translated == 9:
+    print("Nueve")
+if translated == 10:
+    print("Dies")
