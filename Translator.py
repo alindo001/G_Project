fe7f874df85b934de8adf4f567cbac099d4359c8
Andres <andreslindo20@gmail.com>
2021-12-10 13:26:19 -0500

    Update

--- Translator.py
+++ Translator.py
@@ -1,24 +1,12 @@
-numbersdict = {
-          "one": "uno",
-          "two": "dos",
-        "three": "tres",
-         "four": "cuatro",
-         "five": "cinco",
-          "six": "seis",
-        "seven": "siete",
-        "eight": "ocho",
-         "nine": "nueve",
-          "ten": "dies",
-
-}
+from numbersdict import *
 
 
 
-user_input = (input("Enter a number 1 through 10 to output the Spanish translation or type exit to end the program >>>"))
+user_input = (input("Enter a number 1 through 20 to output the Spanish translation or type exit to end the program >>>"))
 
 while user_input != "exit":
 
     if user_input in numbersdict:
         print(numbersdict[user_input])
 
-    user_input = (input("Enter a number 1 through 10 to output the Spanish translation "))
+    user_input = (input("Enter a number 1 through 20 to output the Spanish translation "))
---
+++ numbersdict.py
@@ -0,0 +1,25 @@
+numbersdict = {
+    "zero": "cero",
+    "one": "uno",
+    "two": "dos",
+    "three": "tres",
+    "four": "cuatro",
+    "five": "cinco",
+    "six": "seis",
+    "seven": "siete",
+    "eight": "ocho",
+    "nine": "nueve",
+    "ten": "dies",
+    "eleven": "once",
+    "twelve": "doce",
+    "thirteen": "trece",
+    "fourteen": "catorce",
+    "fifteen": "quince",
+    "sixteen": "diesiseis",
+    "seventeen": "diesisiete",
+    "eighteen": "diesiocho",
+    "nineteen": "diecinueve",
+    "twenty": "veinte",
+
+
+}
